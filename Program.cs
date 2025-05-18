f564e07047c1cfd7e250eabd36f782c497344f05
sophie-lanuit <sofigaran-ipt28@lll.kpi.ua>
2025-05-18 23:54:17 +0300

    mutex

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+
+class Program
+{
+    static Mutex mutex;
+
+    static void Main(string[] args)
+    {
+        const string mutexName = "Global\\MyUniqueAppMutex"; 
+
+        bool isNewInstance;
+
+        mutex = new Mutex(true, mutexName, out isNewInstance);
+
+        if (!isNewInstance)
+        {
+            Console.WriteLine("Other example is started. Finishing the program.");
+            return;
+        }
+
+        Console.WriteLine("Executing the program, click Enter to finish.");
+        Console.ReadLine();
+
+        mutex.ReleaseMutex();
+    }
+}
